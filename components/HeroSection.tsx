eb910a9ef2898fa032937d3c8843353c3c9477cd
ChinmayNoob <chinmaysk0408@gmail.com>
2025-01-12 01:13:19 +0530

    navigation bar for desktop is done

---
+++ components/HeroSection.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+    return (
+        <section className='flex justify-between items-center mx-auto max-w-[1440px] px-6 lg:px-20 3xl:px-0 relative py-5 w-full'>
+            <div>
+                HeroSection
+            </div>
+        </section>
+    )
+}
+
+export default HeroSection
